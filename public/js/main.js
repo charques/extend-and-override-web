8109abe859544972b399eeeff507e163076736a8
charques <carloshernandezarques@gmail.com>
2017-03-07 22:45:21 +0100

    new native bridge

--- public/js/main.js
+++ public/js/main.js
@@ -2,18 +2,41 @@
  * Created by cfhernandez on 3/3/17.
  */
 
-function callNativeApp () {
+var clicks = 0;
+
+function getBridge() {
     try {
-        webkit.messageHandlers.callbackHandler.postMessage("Hello from JavaScript");
+        return webkit.messageHandlers.native;
     } catch(err) {
-        console.log('The native context does not exist yet');
+        return null;
     }
 }
 
-setTimeout(function () {
-    callNativeApp();
-}, 5000);
+function incrementCount(){
+    var bridge = getBridge();
+
+    if (bridge) {
+        var message = {
+            "cmd": "increment", "count": clicks, "callbackFunc": function (responseAsJSON) {
+                var response = JSON.parse(responseAsJSON);
+                clicks = response['count'];
+                document.querySelector("#counter").innerText = "Count is " + clicks;
+            }.toString()
+        };
+        bridge.postMessage(message);
+    }
+    else {
+        console.log('The native context does not exist');
+    }
+}
 
 function customLogo() {
-    document.querySelector('.lang-logo').style.background = "none";
+    document.querySelector('.lang-logo').style.background = 'none';
 }
+
+function init () {
+    document.querySelector('#increment-btn').addEventListener('click', incrementCount);
+};
+
+document.addEventListener("DOMContentLoaded", init);
+
